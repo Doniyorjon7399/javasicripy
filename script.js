bf7cfb505b24e39957e87c4c4171a36d532cd809
Doniyorjon7399 <zokirovdoniyorjon2001@gmail.com>
2024-12-17 18:22:12 +0500

    4-versiyasi

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("salom dunyo");
+// masala
+// const input = "JavaScript is an amazing programming language";
+// const natija = input.split(" ").reduce((item, currenvalue) => {
+//   if (item.length < currenvalue.split("").length) {
+//     item = currenvalue;
+//   }
+//   return item;
+// });
+// const resoult = [natija, natija.length];
+// console.log(resoult);
+// masala
+// const arr1 = [1, 2, 3, 4, 7],
+//   arr2 = [3, 4, 5, 6];
+// const arr1Clone = arr1.length > arr2.length ? arr1 : arr2;
+// const arr2Clone = arr1.length < arr2.length ? arr1 : arr2;
+// const resoult = [];
+// arr1Clone.forEach((item) => {
+//   for (let elelment of arr2Clone) {
+//     if (item === elelment) {
+//       resoult.push(elelment);
+//     }
+//   }
+// });
+// console.log(resoult);
